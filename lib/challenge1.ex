b51377e9f07ed83a96970d9304ba049acd8565e1
Gabriel Lima <gabrieldesouzalima@gmail.com>
2022-04-15 16:45:29 -0300

    first commit

---
+++ lib/challenge1.ex
@@ -0,0 +1,18 @@
+defmodule Challenge1 do
+  @moduledoc """
+  Documentation for `Challenge1`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Challenge1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/challenge1_test.exs
@@ -0,0 +1,8 @@
+defmodule Challenge1Test do
+  use ExUnit.Case
+  doctest Challenge1
+
+  test "greets the world" do
+    assert Challenge1.hello() == :world
+  end
+end
