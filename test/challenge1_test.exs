6ff35dcaa2c0936e41ff8b05a141e04f03fd7b68
Gabriel Lima <gabrieldesouzalima@gmail.com>
2022-04-15 16:51:11 -0300

    test template

--- test/challenge1_test.exs
+++ test/challenge1_test.exs
@@ -1,8 +1,21 @@
-defmodule Challenge1Test do
+defmodule ListLengthTest do
   use ExUnit.Case
-  doctest Challenge1
 
-  test "greets the world" do
-    assert Challenge1.hello() == :world
+  describe "call/1" do
+    test "count list elements" do
+      list1 = [1, 2, 3, 4, 5, 6]
+      list2 = [1, 2]
+      list3 = [1, 2, 3, 4]
+
+      assert ListLength.call(list1) == 6
+      assert ListLength.call(list2) == 2
+      assert ListLength.call(list3) == 4
+    end
+
+    test "passing an empty list" do
+      list = []
+
+      assert ListLength.call(list) == 0
+    end
   end
 end
